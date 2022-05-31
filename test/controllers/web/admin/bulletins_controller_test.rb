04e793882dd3c0b201cca9c2e9981686ad3e9ed4
alexey <alexey-melekhov1989@yandex.ru>
2022-05-31 13:35:43 +0500

    delete odd tests

--- test/controllers/web/admin/bulletins_controller_test.rb
+++ test/controllers/web/admin/bulletins_controller_test.rb
@@ -9,20 +9,6 @@ class Web::Admin::BulletinsControllerTest < ActionDispatch::IntegrationTest
     @bulletin = bulletins :one
   end
 
-  test 'signed admin can get index' do
-    sign_in @admin
-
-    get admin_bulletins_url
-    assert_response :success
-  end
-
-  test 'signin user cant get index' do
-    sign_in @user
-
-    get admin_bulletins_url
-    assert_redirected_to root_path
-  end
-
   test 'guest cant get index' do
     get admin_bulletins_url
     assert_redirected_to root_path
--- test/controllers/web/admin/categories_controller_test.rb
+++ test/controllers/web/admin/categories_controller_test.rb
@@ -10,124 +10,54 @@ class Web::Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
     @attrs = {
       name: Faker::Book.title
     }
-  end
-
-  test 'signed admin can get index' do
     sign_in @admin
-
-    get admin_categories_url
-    assert_response :success
-  end
-
-  test 'signin user cant get index' do
-    sign_in @user
-
-    get admin_categories_url
-    assert_redirected_to root_path
   end
 
   test 'guest cant get index' do
+    sign_in @user
+
     get admin_categories_url
     assert_redirected_to root_path
   end
 
-  test 'signed admin can get new' do
-    sign_in @admin
-
-    get new_admin_category_url
-    assert_response :success
-  end
-
-  test 'signin user cant get new' do
-    sign_in @user
-
-    get new_admin_category_url
-    assert_redirected_to root_path
-  end
-
-  test 'guest cant get new' do
-    get new_admin_category_url
-
-    assert_redirected_to root_path
-  end
-
-  test 'signed admin can get edit' do
-    sign_in @admin
-
+  test 'should get index' do
     get edit_admin_category_url(@category)
     assert_response :success
   end
 
-  test 'signin user cant get edit' do
-    sign_in @user
-
-    get edit_admin_category_url(@category)
-    assert_redirected_to root_path
+  test 'should get new' do
+    get new_admin_category_url
+    assert_response :success
   end
 
-  test 'guest cant get edit' do
+  test 'should get edit' do
     get edit_admin_category_url(@category)
-
-    assert_redirected_to root_path
+    assert_response :success
   end
 
-  test 'signed admin create category' do
-    sign_in @admin
-
+  test 'should create category' do
     post admin_categories_url, params: { category: @attrs }
 
-    category = Category.find_by! name: @attrs[:name]
+    category = Category.find_by! @attrs
 
     assert { category }
     assert_redirected_to admin_categories_url
   end
 
-  test 'signin user cant create category' do
-    sign_in @user
-
-    post admin_categories_url, params: { category: @attrs }
-
-    category = Category.find_by name: @attrs[:name]
-
-    assert { category.nil? }
-    assert_redirected_to root_path
-  end
-
-  test 'guest cant create category' do
-    post admin_categories_url, params: { category: @attrs }
-
-    assert_redirected_to root_path
-  end
-
-  test 'signed admin can update category' do
-    sign_in @admin
-
+  test 'should update category' do
     patch admin_category_url(@category), params: { category: @attrs }
 
-    category = Category.find_by! name: @attrs[:name]
+    category = Category.find_by! @attrs
 
     assert { @category.id == category.id }
     assert_redirected_to admin_categories_url
   end
 
-  test 'signin user cant update category' do
-    sign_in @user
+  test 'should destroy category' do
+    delete admin_category_url(@category)
 
-    patch admin_category_url(@category), params: { category: @attrs }
+    assert { !Category.exists? @category.id }
 
-    category = Category.find_by name: @attrs[:name]
-
-    assert { category.nil? }
-    assert_redirected_to root_path
-  end
-
-  test 'guest cant update category' do
-    patch admin_category_url(@category), params: { category: @attrs }
-
-    category = Category.find_by name: @attrs[:name]
-
-    assert { category.nil? }
-
-    assert_redirected_to root_path
+    assert_redirected_to admin_categories_url
   end
 end
